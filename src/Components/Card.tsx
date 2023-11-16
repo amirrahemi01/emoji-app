4c79a4b878099514bcfe038411c2ca388315d17c
amirrahemi01 <amirrahemi01@gmail.com>
2023-11-17 01:56:34 +0330

    add copy emoji to clipboard

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Emoji } from '../App'
 
 
@@ -6,18 +6,40 @@ type Props = Emoji;
 
 export default function Card({ symbol, keywords, title }: Props) {
   // copy emoji to clipboard
+  const [isCopied, setIsCopied] = useState(false);
+  function copyEmoji(e: string) {
+    return navigator.clipboard.writeText(symbol)
+      .then(() => {
+        // If successful, update the isCopied state value
+        setIsCopied(true);
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 1500);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+
 
   // check emoji exist in cdn server
-  
+
   return (
     <>
       <div className='card-container'
-        onClick={() => console.log(symbol)}
+        // onClick={() => console.log(symbol)}
+        onClick={() => copyEmoji(symbol)}
       >
         {/* <p className='symbol'>https://emojicdn.elk.sh/{symbol}</p> */}
         <img src={`https://emojicdn.elk.sh/${symbol}`} />
         <h2 className='title'>{title}</h2>
         <p className='keyword'>{keywords}</p>
+        <span>{isCopied ? <>
+          <img src={`https://emojicdn.elk.sh/${symbol}`} />
+          <p>Copied!</p>
+        </>
+          : ''}</span>
       </div>
     </>
   )

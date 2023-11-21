173116cf0d7a2418b35b88a0d281a432446b30ef
amirrahemi01 <amirrahemi01@gmail.com>
2023-11-21 23:53:41 +0330

    set global loader

--- src/Hooks/useLoader.tsx
+++ src/Hooks/useLoader.tsx
@@ -8,7 +8,7 @@ export default function useLoader() {
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-      setTimeout(() => setLoading(false), 3000)
+      setTimeout(() => setLoading(false), 2000)
     }, []);
 
     return loading; 
